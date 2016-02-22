e1ee2568b3894f80fe5b5e172f7b79aa52fbdf30
Eliot Berriot <contact@hat.eliotberriot.com>
2016-02-22 19:07:31 +0100

    Splitted project in submodules

---
+++ lifter/managers.py
@@ -0,0 +1,23 @@
+
+class Manager(object):
+    """Used to retrieve / order / filter preferences pretty much as django's ORM managers"""
+
+    def __init__(self, values, queryset_class=None):
+
+        from . import query
+
+        self._values = values
+        self.queryset_class = queryset_class or query.QuerySet
+
+    def get_queryset(self):
+        return self.queryset_class(self._values)
+
+    def all(self):
+        return self.get_queryset().all()
+
+    def __getattr__(self, attr):
+        try:
+            return super(Manager, self).__getattr__(attr)
+        except AttributeError:
+            # Try to proxy on queryset if possible
+            return getattr(self.get_queryset(), attr)
