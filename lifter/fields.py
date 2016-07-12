1cb667528f65f15a2b07a5baf8d962fe52ce6c5a
Eliot Berriot <contact@hat.eliotberriot.com>
2016-07-12 11:58:12 +0200

    Added more fields

--- lifter/fields.py
+++ lifter/fields.py
@@ -1,4 +1,24 @@
+import datetime
 
 
 class Field(object):
-    pass
+    def __init__(self, primary_key=False):
+        self.primary_key = primary_key
+
+class DateField(Field):
+
+    def default_to_python(self, adapter, v, date_format="%Y-%m-%d"):
+        return datetime.datetime.strptime(v, date_format).date()
+
+class CharField(Field):
+    def default_to_python(self, adapter, v):
+        return v
+
+
+class ForeignKey(Field):
+    def __init__(self, model, **kwargs):
+        self.model = model
+        super(ForeignKey, self).__init__(**kwargs)
+
+    def default_to_python(self, adapter, v):
+        return self.adapter.store.adapters[self.model].clean(v)
